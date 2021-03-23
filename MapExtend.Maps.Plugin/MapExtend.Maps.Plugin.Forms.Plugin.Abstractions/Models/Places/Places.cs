90f303a507b06892c25c26c16bf77148fcee5a78
Ndumiso_suspect <73614263+Bhekinkosi12@users.noreply.github.com>
2021-03-23 14:22:59 -0700

    Update Places.cs

--- MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/Models/Places/Places.cs
+++ MapExtend.Maps.Plugin/MapExtend.Maps.Plugin.Forms.Plugin.Abstractions/Models/Places/Places.cs
@@ -10,7 +10,7 @@ namespace Xamarin.Forms.MapExtend.Abstractions.Models.Places
     {
         public object[] html_attributions { get; set; }
         public string next_page_token { get; set; }
-        public Result[] results { get; set; }
+        public Result[,] results { get; set; }
         public string status { get; set; }
     }
 
